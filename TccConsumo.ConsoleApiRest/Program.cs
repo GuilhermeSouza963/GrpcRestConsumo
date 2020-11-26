d8ee923cdc5b24974197437f39bcd9a5c48708eb
guilhermepereira <guilhermepereira@uniaraxa.edu.br>
2020-11-26 18:28:08 -0300

    first commit

---
+++ TccConsumo.ConsoleApiRest/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace TccConsumo.ConsoleApiRest
+{
+    class Program
+    {
+        private const string servidor = "http://localhost:52981";
+        private static async Task ListarProdutos()
+        {
+            Console.WriteLine("Lista de produtos:");
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new System.Uri(servidor);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("api/produto/listar");
+                if (response.IsSuccessStatusCode)
+                {  //GET
+                    var retorno = JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().Result);
+                    foreach (var item in retorno)
+                    {
+                        Console.WriteLine(item.produtoId + " | " + item.descricao + " | ");
+                    }
+
+                }
+            }
+
+                Console.WriteLine();
+        }
+        public static async Task Main(string[] args)
+        {
+            await ListarProdutos();
+        }
+    }
+}
---
+++ TccConsumo.ConsolegRPC/Program.cs
@@ -0,0 +1,40 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TccDev.gRPC.Protos;
+
+namespace TccConsumo.ConsolegRPC
+{
+    class Program
+    {
+        private const string servidor = "https://localhost:5001";
+        private static async Task ListarProdutos(
+            ProdutogRPC.ProdutogRPCClient client)
+        {
+            Console.WriteLine("Lista de produtos:");
+            using (var call = client.Listar(new ListarProdutosRequest()))
+            {
+                var responseStream = call.ResponseStream;
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                var token = cts.Token;
+
+                while (await responseStream.MoveNext(token))
+                {
+                    var dadosProduto = responseStream.Current.Produto;
+                    Console.WriteLine(dadosProduto.ProdutoId + " | " + dadosProduto.Descricao + " | ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+        public static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress(servidor);
+            var client = new ProdutogRPC.ProdutogRPCClient(channel);
+
+            await ListarProdutos(client);
+        }
+    }
+}
